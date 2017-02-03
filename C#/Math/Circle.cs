c43eeeb0832c3e4a7987eb13617c3283d5d08985
Hajar Rashidi <hajarrashidi@gmail.com>
2017-02-04 00:17:16 +0100

    Commented the circumference method

--- C#/Math/Circle.cs
+++ C#/Math/Circle.cs
@@ -3,7 +3,8 @@ using System;
 namespace app
 {
     public class Circle
-    {
+    {   
+        //Calculates the circles circumference.
         public static double circumference(double radius)
         {
             return (2 * Math.PI) * radius;
