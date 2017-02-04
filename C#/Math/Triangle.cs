0ef83fec20637c53639bde9df5eae9f34583808c
Hajar Rashidi <hajarrashidi@gmail.com>
2017-02-04 02:35:00 +0100

    New Method  circumference of a triangle.
    Besides the new method, i also changed the comments grammar.

--- C#/Math/Triangle.cs
+++ C#/Math/Triangle.cs
@@ -4,13 +4,20 @@ namespace app
 {
     public class Triangle
     {
-        //Calculates the triangle area.
+        //Calculates the area of a triangle.
         public static double area(double height, double width) {
 
             return (height * width * 0.5);
    
         }
 
+        //Calculates the circumference of a triangle.
+        public static double circumference(double a, double b, double c)
+        {
+            return a + b + c;
+
+        }
+        
     }
 }
 
