556f21f047fe5908fa06105fd152986e104235d7
Hajar Rashidi <hajarrashidi@gmail.com>
2017-02-04 01:50:12 +0100

    New Method for Circle Area

--- C#/Math/Circle.cs
+++ C#/Math/Circle.cs
@@ -3,7 +3,13 @@ using System;
 namespace app
 {
     public class Circle
-    {   
+    {
+        //Calculates the circles area.
+        public static double area(double radius)
+        {   
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
         //Calculates the circles circumference.
         public static double circumference(double radius)
         {
