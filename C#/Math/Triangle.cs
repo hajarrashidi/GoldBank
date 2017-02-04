4eca3f8d2783a12d469e5c2b3f7cb0675efc5145
Hajar Rashidi <hajarrashidi@gmail.com>
2017-02-04 02:19:42 +0100

    Create a new class called and added a method

---
+++ C#/Math/Triangle.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace app
+{
+    public class Triangle
+    {
+        //Calculates the triangle area.
+        public static double area(double height, double width) {
+
+            return (height * width * 0.5);
+   
+        }
+
+    }
+}
+
+
