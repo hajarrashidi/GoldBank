257c07c860764922ec8e1ff0395e73d0ddd6584b
Hajar Rashidi <hajarrashidi@gmail.com>
2017-02-09 08:55:23 -0800

    Capitalization Conventions
    .NET Conventions
    https://msdn.microsoft.com/en-us/library/ms229043.aspx

--- C#/Math/Triangle.cs
+++ C#/Math/Triangle.cs
@@ -5,17 +5,15 @@ namespace app
     public class Triangle
     {
         //Calculates the area of a triangle.
-        public static double area(double height, double width) {
-
+        public static double Area(double height, double width)
+        {
             return (height * width * 0.5);
-   
         }
 
         //Calculates the circumference of a triangle.
-        public static double circumference(double a, double b, double c)
+        public static double Circumference(double a, double b, double c)
         {
             return a + b + c;
-
         }
         
     }
