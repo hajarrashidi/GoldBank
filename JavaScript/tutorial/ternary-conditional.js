3e6fa5497fb99bf2895fd78ed64e80ed0b494a2d
Hajar Rashidi <hajarrashidi@gmail.com>
2017-03-16 00:08:07 +0100

    commented code

--- JavaScript/tutorial/ternary-conditional.js
+++ JavaScript/tutorial/ternary-conditional.js
@@ -1,5 +1,10 @@
+// Example
+
+// set to false
 var sleeping = false;
 
+// if true: Zzz || false: Go to work
 var whatShouldIDo = sleeping ? "Zzz..." : 'Go to work';
 
+//returns Go To Work
 console.log(whatShouldIDo);
