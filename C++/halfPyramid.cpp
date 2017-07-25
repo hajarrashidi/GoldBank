026e6e36d74ac65203c002d199e25d210e683df9
Hajar Rashidi <hajarrashidi@gmail.com>
2017-07-25 09:49:19 +0200

    some few changes

--- C++/halfPyramid.cpp
+++ C++/halfPyramid.cpp
@@ -6,16 +6,14 @@ using namespace std;
 
 int main()
 {
-
     for(int row = 0; row < 10; row++){
         for(int col = 0; col < row; col++){
             cout << "*";
         }
         cout << "\n";
     }
-
-
-    getchar();
+    
+    getchar(); // to pause the program
     return 0;
 }
 
